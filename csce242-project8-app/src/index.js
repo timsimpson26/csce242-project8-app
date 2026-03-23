a779cbcf01fa09d9bd5ba823e7376ecdca9e3f39
Tim Simpson <timothyms2006@gmail.com>
2026-03-22 20:00:05 -0400

    setup

---
+++ csce242-project8-app/src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client';
+import {Routes, Route, BrowserRouter } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import NoPage from "./pages/nopage";
+
+export default function App() {
+  return (
+    <BrowserRouter basename = {process.env.PUBLIC_URL_}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
